2e96d36c2766510393d987a3c39f03ae29ac79b1
pouryawJS <pourya.bu@gmail.com>
2025-04-04 23:01:26 +0330

    fix: change the method of login api

--- backend/modules/Auth/auth.routes.js
+++ backend/modules/Auth/auth.routes.js
@@ -4,7 +4,7 @@ const controller = require("./auth.controller.js");
 const router = express.Router();
 
 router.route("/register").post(controller.register);
-router.route("/login").get(controller.login);
+router.route("/login").post(controller.login);
 router.route("/refresh-token").post(controller.refreshToken);
 
 module.exports = router;
