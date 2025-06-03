c1ca583c0b21d996efbbc6f87f18e977ef9823fd
pouryawJS <pourya.bu@gmail.com>
2025-06-03 18:24:49 +0330

    fix: change otp model structure

--- backend/models/Otp.js
+++ backend/models/Otp.js
@@ -2,21 +2,31 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema(
 	{
-		email: String,
-		code: Number,
-		userAttempts: {
+		token: {
+			type: String,
+			required: true,
+		},
+		code: {
+			type: Number,
+			required: true,
+		},
+		attempts: {
 			type: Number,
 			default: 0, // max is 3
 		},
 		expiresAt: {
-			type: Number,
+			type: Date,
 			required: true,
+			default: () => new Date(Date.now() + 3 * 60 * 1000),
+			expires: 0,
 		},
 	},
 	{
 		versionKey: false,
 	}
 );
+
+schema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 const model = mongoose.model("otp", schema);
 
